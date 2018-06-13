4beba1ea2feefd7bf49bfcf3ee9d6c289eb51f4d
starsoccer <1938666+starsoccer@users.noreply.github.com>
2018-06-13 19:54:38 -0400

    Created Toggle component

---
+++ components/Toggle/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+export interface IToggleProps {
+    className?: string;
+    defaultValue?: boolean;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export class Toggle extends React.Component<IToggleProps> {
+    public render() {
+        return (
+            <label className='toggle'>
+                <link rel='stylesheet' type='text/css' href='./components/Toggle/index.css' />
+                <input type='checkbox' onChange={this.props.onChange} defaultChecked={this.props.defaultValue}/>
+                <span className='slider'></span>
+            </label>
+        );
+    }
+}
