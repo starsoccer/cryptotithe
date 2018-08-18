db4d264a42698ab558665b7437dba37b98717b26
starsoccer <1938666+starsoccer@users.noreply.github.com>
2018-08-18 19:33:20 -0400

    Added timeline feature for trades and made some small color tweaks

--- components/Tabs/ViewTradesTab/index.tsx
+++ components/Tabs/ViewTradesTab/index.tsx
@@ -5,18 +5,26 @@ import { FiatRateMethod, IPartialSavedData, ISavedData, ITrade, ITradeWithFiatRa
 import Button from '../../Button';
 import { Loader } from '../../Loader';
 import { TradesTable } from '../../TradesTable';
+import { Timeline } from '../../Timeline';
 
 export interface IViewTradesTabProp {
     savedData: ISavedData;
     save(data: IPartialSavedData): Promise<boolean>;
 }
 
-export class ViewTradesTab extends React.Component<IViewTradesTabProp, {processing: boolean}> {
+export interface IViewTradesTabState {
+    processing: boolean;
+    tradeTable: boolean;
+}
+
+
+export class ViewTradesTab extends React.Component<IViewTradesTabProp, IViewTradesTabState> {
 
     public constructor(props: IViewTradesTabProp) {
         super(props);
         this.state = {
             processing: false,
+            tradeTable: false,
         };
     }
 
@@ -35,24 +43,36 @@ export class ViewTradesTab extends React.Component<IViewTradesTabProp, {processi
         this.setState({processing: false});
     }
 
+    public tradeTable = () => {
+        this.setState({
+            tradeTable: !this.state.tradeTable,
+        });
+    }
+
     public render() {
         return (
             <div className='viewTrades'>
                 <h3 className='tc'>Trades</h3>
                 <hr className='center w-50' />
-                <div className='tc center'>
-                <Button label='Refresh Trade Data' onClick={this.refetchFiatRate}/>
-                {this.state.processing ?
-                    <Loader />
-                :
-                    this.props.savedData.trades.length > 0 ?
-                        <TradesTable
-                            trades={this.props.savedData.trades}
-                            save={this.save}
-                        />
+                <div className='tc center pb2'>
+                    <Button label='Refresh Trade Data' onClick={this.refetchFiatRate}/>
+                    <Button label='Trade Table' onClick={this.tradeTable}/>
+                </div>
+                <div className="tc center">
+                    {this.state.processing ?
+                        <Loader />
                     :
-                        <h3 className='tc'>No Trades <i className='fa fa-frown-o'></i></h3>
-                }
+                        this.state.tradeTable ?
+                            this.props.savedData.trades.length > 0 ?
+                                <TradesTable
+                                    trades={this.props.savedData.trades}
+                                    save={this.save}
+                                />
+                            :
+                                <h3 className='tc'>No Trades <i className='fa fa-frown-o'></i></h3>
+                        :
+                            <Timeline trades={this.props.savedData.trades.reverse()}/>
+                    }
                 </div>
             </div>
         );
---
+++ components/Timeline/index.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { ITrade } from '../../src/types';
+export interface ITimelineProp {
+    trades: ITrade[]
+}
+
+export class Timeline extends React.PureComponent<ITimelineProp> {
+    public render() {
+        return (
+            <div className='timeline relative center'>
+                <link rel='stylesheet' type='text/css' href='./components/Timeline/index.css' />
+                {this.props.trades.map((trade, index) =>
+                    <div className={`container ${index % 2 === 0 ? 'left' : 'right'}`} key={trade.id}>
+                        <div className='pv2 ph3 bg-white relative br2'>
+                            <h2>Sold {trade.amountSold.toFixed(8)} {trade.soldCurrency}</h2>
+                            <h4>Got {(trade.amountSold / trade.rate).toFixed(8)} {trade.boughtCurrency}</h4>
+                            <p>{trade.rate.toFixed(8)} rate on {trade.exchange || 'Unknown'}</p>
+                            <h6>{new Date(trade.date).toUTCString()}</h6>
+                        </div>
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
