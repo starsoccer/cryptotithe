9421cec82b84116c6d8c14da5c4ec73ec259ee96
starsoccer <1938666+starsoccer@users.noreply.github.com>
2018-08-25 19:45:01 -0400

    Small tweaks to popup styling

--- components/Popup/index.tsx
+++ components/Popup/index.tsx
@@ -10,8 +10,8 @@ export default class Popup extends React.PureComponent<IPopupProps> {
         return (
             <div className='popup'>
                 <link rel='stylesheet' type='text/css' href='./components/Popup/index.css' />
-                <div className='fixed w-100 h-100 top-0 right-0 left-0 bottom-0 bg-black o-70' />
-                <div className={`dialog absolute w-50 ba b--black bw2 bg-white pa2 ${this.props.className}`}>
+                <div className='fixed w-100 h-100 top-0 right-0 left-0 bottom-0 bg-black o-70 z-1' />
+                <div className={`fixed dialog absolute w-70 ba b--black bw2 bg-white pa2 z-2 ${this.props.className}`}>
                     <i className='fr fa fa-times fa-2x' onClick={this.props.onClose}></i>
                     {this.props.children}
                 </div>
