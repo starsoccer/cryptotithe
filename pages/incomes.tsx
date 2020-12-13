fe8825e61081e84c977914ce992a59e0b4f97df9
starsoccer <1938666+starsoccer@users.noreply.github.com>
2020-12-13 14:32:24 -0500

    move incomes to use download context

--- pages/incomes.tsx
+++ pages/incomes.tsx
@@ -1,27 +1,29 @@
-import {useContext, useState, useEffect} from 'react';
+import {useContext, useState} from 'react';
 import { IncomesTable } from '@components/IncomesTable';
 import getYears from '@utils/getYears';
 import getByYear from '../src/processing/getByYear';
 import calculateIncomesValue from '../src/processing/CalculateIncomesValue';
 import incomesOutput from '../src/output/Incomes';
-import { FileDownload } from '@components/FileDownload';
 import Button from '@components/Button';
 import SavedDataConext from '@contexts/savedData';
+import DownloadContext from '@contexts/download';
 
 const CalculateIncomes = () => {
     const {savedData} = useContext(SavedDataConext);
+    const {setDownloadInfo} = useContext(DownloadContext);
     const [year, setYear] = useState(0);
-    const [shouldDownload, setShouldDownload] = useState(false);
 
     const years = getYears(savedData.incomes);
     const filteredIncomes = getByYear(savedData.incomes, year);
     const {incomes, totalValue} = calculateIncomesValue(filteredIncomes);
 
-    useEffect(() => {
-        if (shouldDownload) {
-            setShouldDownload(false);
-        }
-    }, [shouldDownload]);
+    const onDownloadClick = () => {
+        setDownloadInfo({
+            data: incomesOutput(incomes),
+            fileName: "incomes.csv",
+            download: true,
+        });
+    }
 
     return (
         <div className="calculate-income">
@@ -43,16 +45,10 @@ const CalculateIncomes = () => {
                 {!!totalValue &&
                     <div className="tc">
                         <h4 className="tc">Value: {totalValue}</h4>
-                        <Button label="Download" onClick={() => setShouldDownload(true)} />
+                        <Button label="Download" onClick={onDownloadClick} />
                     </div>
                 }
             </div>
-
-            <FileDownload
-                data={incomesOutput(incomes)}
-                fileName="incomes.csv"
-                download={shouldDownload}
-            />
 
             {!!year && !!incomes.length &&
                 <IncomesTable
